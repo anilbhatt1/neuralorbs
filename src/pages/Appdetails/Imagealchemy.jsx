88bc1ac7e889a6e303d5afcbfd3f6f9da8fecbe2
anilbhatt1 <anilaknb@gmail.com>
2024-02-05 09:10:49 +0530

    Modified Imagealchemy links

--- src/pages/Appdetails/Imagealchemy.jsx
+++ src/pages/Appdetails/Imagealchemy.jsx
@@ -1,8 +1,9 @@
 // Imagealchemy.jsx
 import React from 'react';
 import { Typography, Link, Avatar, Box} from '@mui/material';
-const hfurl = "https://huggingface.co/spaces/anilbhatt1/ImageAlchemy-StableDiffusion";
+const hfurl = "https://huggingface.co/spaces/neuralorbs/ImageAlchemy-StableDiffusion";
 const githuburl = "https://github.com/anilbhatt1/ERA1_S20_Stable_Diffusion";
+const yturl = "https://youtu.be/cVG8u24Z3_c";
 
 const Imagealchemy = () => {
   return (
@@ -44,7 +45,23 @@ const Imagealchemy = () => {
                     Github
                   </Link> 
               </Typography>                
-          </Box>                                           
+          </Box>        
+          <Box sx={{ display: 'inline-flex' }} m={4}>
+              <Avatar
+                variant="square"
+                src='/images/imagealchemy_youtube.png'
+                alt='Jane Doe'
+                sx={{bgcolor: 'primary.main', height:100, width:100}}
+              />   
+              <Typography variant="h4" style={{ fontFamily: "Crimson Text, serif"}} m={4}>
+                  View youtube video demo : 
+                  <Link href={yturl} 
+                        underline="none" variant="h4" target="_blank" rel="noopener noreferrer"
+                  >
+                    Youtube Demo Video (2:06 mins)
+                  </Link> 
+              </Typography>                
+          </Box>  
       </div>                          
   );
 };
